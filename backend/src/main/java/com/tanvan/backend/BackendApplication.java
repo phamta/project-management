6495e879df19f75a10af6f08c66e7d46244a4d37
Tanvan <tanvan.13022014@gmail.com>
2026-09-15 00:08:46 +0700

    Add log

--- backend/src/main/java/com/tanvan/backend/BackendApplication.java
+++ backend/src/main/java/com/tanvan/backend/BackendApplication.java
@@ -3,10 +3,15 @@ package com.tanvan.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class BackendApplication {
 
     public static void main(String[] args) {
+        log.info("Server running at: http://localhost:8080");
+        log.info("API Docs: http://localhost:8080/swagger-ui/index.html");
         SpringApplication.run(BackendApplication.class, args);
     }
 
